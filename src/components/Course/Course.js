22f32e4efc783a1440a31c1c829a6b2f79e44411
Le Minh Duc <minhduc23111997@gmail.com>
2022-10-17 09:29:34 +0700

    complete responsive

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import "./Course.css"
+import React from "react";
+import "./Course.css";
 
-function Course({imgUrl}) {
+function Course({ imgUrl }) {
   return (
-    <div className='course'>
-      <p className='course-title'>Lorem ipsum dolor sit amet</p>
-      <div className='course-content'>
-      <img className='course-img' src={imgUrl} alt="course-img" /> 
- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique consequat placerat. Vestibulum auctor pellentesque sem, eu posuere erat hendrerit quis. Maecenas vel consequat turpis. Nam facilisis, ligula in mattis sodales, augue justo tristique nulla, sed lacinia ante eros ut mi. Morbi vitae diam augue. Aliquam vel mauris a nibh auctor commodo. Praesent et nisi eu justo eleifend convallis. Quisque suscipit maximus vestibulum. Phasellus congue mollis orci, sit amet luctus augue tristique eu. Donec vulputate odio neque, sed semper turpis pellentesque a.
-
+    <div className="course">
+      <p className="course-title">Lorem ipsum dolor sit amet</p>
+      <div className="course-content">
+        <img className="course-img" src={imgUrl} alt="course-img" />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique
+        consequat placerat. Vestibulum auctor pellentesque sem, eu posuere erat
+        hendrerit quis. Maecenas vel consequat turpis. Nam facilisis, ligula in
+        mattis sodales, augue justo tristique nulla, sed lacinia ante eros ut
+        mi. Morbi vitae diam augue. Aliquam vel mauris a nibh auctor commodo.
+        Praesent et nisi eu justo eleifend convallis. Quisque suscipit maximus
+        vestibulum.
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Course;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,14 +2,25 @@ import React from "react";
 
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Content from "../../components/Content/Content";
+import Footer from "../../components/Footer/Footer";
 
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
-      <Sidebar />
-      <Content />
+      <div className="sidebar-wrapper">
+        <Sidebar />
+      </div>
+      <div className="content-wrapper">
+        <div>
+        <Content />
+        </div>
+        <div>
+        <Footer />
+        </div>
+        
+      </div>
     </div>
   );
 }
